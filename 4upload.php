f97aca37625a92a283d5bc2740fddb3d7679b29e
Ayati30 <42727472+Ayati30@users.noreply.github.com>
2018-10-11 20:21:13 +0700

    Add files via upload

---
+++ 4upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form method="post" action="prossesupload.php" enctype="multipart/form-data">
+			<tr><td><br></td></tr>
+			<tr>
+				<td><input type="file" name="file" id="file"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="upload"></td>
+			</tr>
+		</form>
+  </body>
+</html>
